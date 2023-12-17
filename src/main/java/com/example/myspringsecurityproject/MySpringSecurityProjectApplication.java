6a7045f17ae163bbf718d3f4812c1e8f680afea0
Kyungryul lee <leekyung1111@gmail.com>
2023-12-17 19:12:28 +0900

    Initial commit

---
+++ src/main/java/com/example/myspringsecurityproject/MySpringSecurityProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.myspringsecurityproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpringSecurityProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MySpringSecurityProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/myspringsecurityproject/MySpringSecurityProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myspringsecurityproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySpringSecurityProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
